98898b4e2f13f48c68ef3e5e8366ec986822b60e
Saru <133031589+Saruuu24@users.noreply.github.com>
2023-11-22 22:23:43 +0530

    add easter
    asddsadas

--- script7.py
+++ script7.py
@@ -1,77 +1,113 @@
-import tkinter as tk
-from tkinter import ttk
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QComboBox, QTextEdit
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
 import pyttsx3
+from gtts import gTTS
+import os
+import webbrowser
 
-class TextToSpeechApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Text to Speech App")
-        self.root.geometry("400x250")
+class TTSApp(QWidget):
+    def __init__(self):
+        super().__init__()
 
-        # Available voices
-        self.voices = self.get_available_voices()
+        self.init_ui()
 
-        if not self.voices:
-            tk.messagebox.showerror("Error", "No voices available.")
-            self.root.destroy()
-            return
+    def init_ui(self):
+        # Set up the main layout
+        main_layout = QVBoxLayout()
 
-        self.create_widgets()
+        # Add cloud icon in the top left corner
+        cloud_icon = QLabel(self)
+        cloud_icon.setPixmap(QIcon('cloud_icon.png').pixmap(25, 25))
+        cloud_icon.mousePressEvent = self.icon_clicked
+        main_layout.addWidget(cloud_icon, alignment=Qt.AlignTop| Qt.AlignLeft)
 
-    def create_widgets(self):
-        # Text input
-        self.text_label = ttk.Label(self.root, text="Enter Text:")
-        self.text_label.pack(pady=10)
-
-        self.text_entry = ttk.Entry(self.root, width=40)
-        self.text_entry.pack(pady=10)
+        # Text input area
+        self.text_edit = QTextEdit(self)
+        main_layout.addWidget(self.text_edit)
 
         # Voice selection
-        self.voice_label = ttk.Label(self.root, text="Select Voice:")
-        self.voice_label.pack(pady=5)
+        voice_layout = QHBoxLayout()
+        voice_label = QLabel("Select Voice:")
+        self.voice_combobox = QComboBox(self)
+        self.populate_voices()
+        voice_layout.addWidget(voice_label)
+        voice_layout.addWidget(self.voice_combobox)
+        main_layout.addLayout(voice_layout)
 
-        self.voice_combobox = ttk.Combobox(self.root, values=self.voices, state="readonly")
-        self.voice_combobox.pack(pady=10)
-        self.voice_combobox.set(self.voices[0])  # Set the default voice
+        # Buttons
+        button_layout = QHBoxLayout()
+        self.speak_button = QPushButton("Speak", self)
+        self.speak_button.clicked.connect(self.speak_text)
+        button_layout.addWidget(self.speak_button)
 
-        # Speak button
-        self.speak_button = ttk.Button(self.root, text="Speak", command=self.speak_text)
-        self.speak_button.pack(pady=10)
+        # Add layouts to the main layout
+        main_layout.addLayout(button_layout)
 
-    def get_available_voices(self):
-        # Get the available voices
+        # Set the main layout
+        self.setLayout(main_layout)
+
+        # Set the window properties
+        self.setWindowTitle('Text-to-Speech App')
+        self.setGeometry(100, 100, 600, 400)
+
+        # Show the window
+        self.show()
+
+        # Counter for icon clicks
+        self.icon_click_count = 0
+
+    def populate_voices(self):
+        # Clear the combobox before populating
+        self.voice_combobox.clear()
+
+        # Use pyttsx3 to get available voices
         engine = pyttsx3.init()
-        voices = engine.getProperty('voices')
-        return [f"{voice.name} ({voice.languages[0]}, {voice.gender})" for voice in voices] if voices else []
+        pyttsx3_voices = engine.getProperty('voices')
+
+        # Use gTTS to get available voices
+        gtts_voices = ['en', 'es', 'fr']  # Add language codes as needed
+
+        # Populate the ComboBox with pyttsx3 voices
+        for voice in pyttsx3_voices:
+            self.voice_combobox.addItem(f"pyttsx3: {voice.name}")
+
+        # Populate the ComboBox with gTTS voices
+        for voice in gtts_voices:
+            self.voice_combobox.addItem(f"gTTS: {voice}")
 
     def speak_text(self):
-        text_to_speak = self.text_entry.get()
-        selected_voice_name = self.voice_combobox.get().split(" (")[0]
+        # Get the selected voice
+        selected_voice = self.voice_combobox.currentText()
 
-        # Initialize the text-to-speech engine
-        engine = pyttsx3.init()
+        # Determine the TTS engine based on the voice selection
+        if selected_voice.startswith("pyttsx3"):
+            engine = pyttsx3.init()
+            engine.setProperty('voice', selected_voice.split(":")[1].strip())
+            text_to_speak = self.text_edit.toPlainText()
+            engine.say(text_to_speak)
+            engine.runAndWait()
+        elif selected_voice.startswith("gTTS"):
+            language_code = selected_voice.split(":")[1].strip()
+            text_to_speak = self.text_edit.toPlainText()
+            tts = gTTS(text=text_to_speak, lang=language_code, slow=False)
+            tts.save("temp.mp3")
+            os.system("start temp.mp3")  # Open the default media player to play the audio
+        else:
+            print("Unknown TTS engine")
 
-        # Set the selected voice
-        for voice in engine.getProperty('voices'):
-            if voice.name == selected_voice_name:
-                engine.setProperty('voice', voice.id)
-                break
+    def icon_clicked(self, event):
+        self.icon_click_count += 1
 
-        # Set properties (optional)
-        engine.setProperty('rate', 150)  # Speed of speech
+        if self.icon_click_count == 24:
+            self.open_website()
 
-        # Speak the text
-        engine.say(text_to_speak)
+    def open_website(self):
+        # Open a website in the default web browser
+        webbrowser.open("https://www.youtube.com/watch?v=mVubYBRajfw")
 
-        # Wait for the speech to finish
-        engine.runAndWait()
-
-if __name__ == "__main__":
-    root = tk.Tk()
-
-    # Use a different theme (e.g., "clam")
-    style = ttk.Style()
-    style.theme_use("clam")
-
-    app = TextToSpeechApp(root)
-    root.mainloop()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    tts_app = TTSApp()
+    sys.exit(app.exec_())
