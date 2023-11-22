1f1494d4f390a308048377198bad56b842f11bb4
Saru <133031589+Saruuu24@users.noreply.github.com>
2023-11-22 21:10:50 +0530

    asdasd
    adasd

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel
-from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QGridLayout, QPushButton, QLabel
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import Qt, QSize
+
 class FileOpenerApp(QWidget):
     def __init__(self):
         super().__init__()
@@ -12,44 +13,64 @@ class FileOpenerApp(QWidget):
         # Set up the layout
         main_layout = QVBoxLayout()
 
-        # Add an icon for the program
-        self.setWindowIcon(QIcon("logo.png"))
+        # Add a centered title
+        title_label = QLabel("Zara - A simple app for students", self)
+        title_label.setAlignment(Qt.AlignCenter)
+        title_font = QFont("Gabriola", 20, QFont.Bold)
+        title_label.setFont(title_font)
+        main_layout.addWidget(title_label)
 
-        # Add a QLabel for displaying a GIF
-        gif_label = QLabel(self)
-        gif_pixmap = QPixmap("zara.gif")
-        gif_label.setPixmap(gif_pixmap)
-        gif_label.setAlignment(Qt.AlignCenter)
+        # Add buttons in rows of four
+        button_grid_layout = QGridLayout()
 
         # Define the button names, icons, and file names
         button_info = [
-            ("Button 1", "icon1.png", "script1.py"),
-            ("Button 2", "icon2.png", "script2.py"),
-            ("Button 3", "icon3.png", "script3.py"),
-            ("Button 4", "icon4.png", "script4.py"),
-            ("Button 5", "icon5.png", "script5.py"),
-            ("Button 6", "icon6.png", "script6.py"),
-            ("Button 7", "icon7.png", "script7.py"),
-            ("Button 8", "icon8.png", "script8.py"),
+            ("Locally save videos", "icon1.png", "script1.py"),
+            ("Focus Mode", "icon2.png", "script2.py"),
+            ("Sticky notes", "icon3.png", "script3.py"),
+            ("Timetable generator", "icon4.png", "script4.py"),
+            ("Summarize news articles", "icon5.png", "script5.py"),
+            ("Create todo list", "icon6.png", "script6.py"),
+            ("Voice assistant", "icon7.png", "script7.py"),
+            ("Morse Code decrypter (its useless)", "icon8.png", "script8.py"),
         ]
-
+        row, col = 0, 0
         for name, icon_file, file_name in button_info:
-            button = QPushButton(name, self)
+            button = QPushButton(self)
             icon = QIcon(icon_file)
             button.setIcon(icon)
-            button.setIconSize(icon.actualSize(icon.availableSizes()[0]))  # Set the icon size
+            button.setIconSize(QSize(256, 256))  # Set the custom icon size
             button.clicked.connect(lambda _, file=file_name: self.openFile(file))
-            main_layout.addWidget(button)
 
-        # Add the GIF label above the first row of buttons
-        main_layout.addWidget(gif_label)
+            # Set up a QLabel for the button text below the icon
+            label = QLabel(name, self)
+            label.setAlignment(Qt.AlignCenter)
+
+            # Remove the border around the button
+            button.setStyleSheet("border: none;")
+
+            # Add the icon and text to the layout
+            button_layout = QVBoxLayout()
+            button_layout.addWidget(button)
+            button_layout.addWidget(label)
+
+            button_grid_layout.addLayout(button_layout, row, col)
+
+            col += 1
+            if col > 3:
+                col = 0
+                row += 1
+
+        main_layout.addLayout(button_grid_layout)
 
         self.setLayout(main_layout)
 
         # Set up the main window
-        self.setGeometry(300, 300, 400, 400)
-        self.setWindowTitle('File Opener App')
-        self.show()
+        self.setGeometry(300, 300, 600, 400)
+        self.setWindowTitle('Zara')
+
+        # Apply the "Windows" style theme
+        QApplication.setStyle("Windows")
 
     def openFile(self, file_name):
         # Check if the file exists in the program directory
@@ -61,4 +82,5 @@ class FileOpenerApp(QWidget):
 if __name__ == '__main__':
     app = QApplication([])
     ex = FileOpenerApp()
+    ex.show()
     app.exec_()
